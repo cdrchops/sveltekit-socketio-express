fa94aa22550b9fec1df9064a2d22d059491dc496
cdrchops <cdrchops@users.noreply.github.com>
2022-09-24 15:03:08 -0500

    removed server directory

--- server/grails-app/controllers/server/CrewController.groovy
+++
@@ -1,7 +0,0 @@
-package server
-
-//the crew is a specific roster for a Ship
-// where a player can belong to many crews
-// and each ship can have many crews
-class CrewController {
-}
--- server/grails-app/domain/server/EventMessage.groovy
+++
@@ -1,7 +0,0 @@
-package server
-
-class EventMessage {
-
-    static constraints = {
-    }
-}
